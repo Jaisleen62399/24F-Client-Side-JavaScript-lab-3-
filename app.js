fce5f2d651302a44065c6a4426240e2722a94064
Jaisleen62399 <156478290+Jaisleen62399@users.noreply.github.com>
2024-11-12 23:32:59 -0500

    Update app.js
    constructor for car added

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 
+// Constructor for ToyCar
+function ToyCar(model, brand, scale, color, dimensions, material, price, features, stock, releaseYear, weight, manufacturerCountry, vehicleType, colorOptions, packaging, toyFeatures, customerRating) {
+    this.model = model;
+    this.brand = brand;
+    this.scale = scale;
+    this.color = color;
+    this.dimensions = dimensions;
+    this.material = material;
+    this.price = price;
+    this.features = features;
+    this.stock = stock;
+    this.releaseYear = releaseYear;
+    this.weight = weight;
+    this.manufacturerCountry = manufacturerCountry;
+    this.vehicleType = vehicleType;
+    this.colorOptions = colorOptions;
+    this.packaging = packaging;
+    this.toyFeatures = toyFeatures;
+    this.customerRating = customerRating;
+  
